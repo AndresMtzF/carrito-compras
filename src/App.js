799dc96d0c480e1fd4c1bb14f09a29add85205e7
Andrés Martínez <andres_mf1@live.com.mx>
2022-08-21 18:29:43 -0500

    estilo y boton agregar productos ✓

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+//Guardar todos los componentes personalizados en un folder 'components' para mantener un orden
+import Productos from './components/Productos';
+import Layout from './components/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    productos: [
+      { name: 'Tomate', price: 25, img: '/productos/tomatoe.png' },
+      { name: 'Cebolla', price: 30, img: '/productos/onion.png' },
+      { name: 'Lechuga', price: 35, img: '/productos/lettuce.png' }
+    ]    
+  }
+
+  render(){
+    return (
+      <div>
+        <Layout>
+          <Productos
+            agregarAlCarro = { () => console.log('AgregarAlCarro')}
+            productos={this.state.productos}
+          />
+        </Layout>        
+      </div>
+    )
+  }
 }
 
 export default App;
